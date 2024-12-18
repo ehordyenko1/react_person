b96a3ae220380d6f8f20c5c5ea3c812e61cc11e5
Yehor Gordiienko <egordienko319@gmail.com>
2024-12-18 23:57:32 +0300

    null instead of space in p

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -2,7 +2,7 @@ export const Person = ({ person }) => (
   <div>
     <h2 className="Person__name">{`My name is ${person.name}`}</h2>
 
-    <p className="Person__age">{person.age ? `I am ${person.age}` : ' '}</p>
+    <p className="Person__age">{person.age ? `I am ${person.age}` : null}</p>
 
     <p className="Person__partner">
       {person.sex === 'm' && person.isMarried
