e2b6690b9d6fe4b23308266d256224382925ce15
Yehor Gordiienko <egordienko319@gmail.com>
2024-12-18 23:54:00 +0300

    func is done

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,16 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <div>
+    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+
+    <p className="Person__age">{person.age ? `I am ${person.age}` : ' '}</p>
+
+    <p className="Person__partner">
+      {person.sex === 'm' && person.isMarried
+        ? `My wife is ${person.partnerName}`
+        : ''}
+      {person.sex === 'f' && person.isMarried
+        ? `My husband is ${person.partnerName}`
+        : ''}
+    </p>
+  </div>
+);
